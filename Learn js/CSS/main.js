21b4cd991f07ed123b2a6f97e649e65e71ad3c38
Eng.Mustafa Eid <you@example.com>
2021-08-19 17:31:31 +0200

    Edit16

---
+++ Learn js/CSS/main.js
@@ -0,0 +1,17 @@
+
+var v1 = 'Eng Mustafa Eid';
+var v2 = 5852;
+var v3 = true;
+document.write(typeof(v3));
+const ip = 48521;
+document.write(ip);
+//this is comment 
+/*
+comments
+comments
+comments
+comments
+comments
+*/
+
+
